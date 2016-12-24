6a2ba2ee98ebeb875df9cab2346b0b4a3eff405c
yangguang <13810491707@163.com>
2016-12-24 22:46:59 +0800

    add valication to title

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,17 +15,20 @@ class GroupsController < ApplicationController
   end
   def create
     @group = Group.new(group_params)
-    @group.save
-
+    if @group.save
       redirect_to groups_path
+    else render :new
+    end
   end
 
   def update
-    @group= Group.find(params[:id])
-    @group.update(group_params)
-
-    redirect_to groups_path, notice: "Update Success"
-  end
+     @group = Group.find(params[:id])
+     if @group.update(group_params)
+       redirect_to groups_path, notice: "Update hahaha "
+     else
+       render :edit
+     end
+   end
 
   def destroy
     @group = Group.find(params[:id])
